3b12d538a1f9f900277cf3c0a16b793cb718ddfa
Rob Hoelz <rhoelz@inoc.com>
2011-08-15 15:34:34 -0500

    initial commit

---
+++ lib/Plack/Middleware/Recorder.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Plack::Middleware::Recorder;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT:  A short description of Plack::Middleware::Recorder
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
